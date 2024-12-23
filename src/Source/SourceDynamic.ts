6768f1001c80fe6a8e799bf76b3b935f7728883a
Eugene Kosukhin <kosuha606@gmail.com>
2024-12-23 12:56:53 +0300

    feat(issue-3): add class source dynamic

---
+++ src/Source/SourceDynamic.ts
@@ -0,0 +1,28 @@
+import { give, GuestType } from "../Guest/Guest";
+import { GuestAwareType } from "../Guest/GuestAware";
+import { PatronPool } from "../Patron/PatronPool";
+import { SourceType } from "./Source";
+
+/**
+ * @url https://kosukhin.github.io/patron.site/#/source-dynamic
+ */
+export class SourceDynamic<T = unknown> implements SourceType<T> {
+  public constructor(
+    private baseGuest: GuestType<T>,
+    private baseGuestAware: GuestAwareType<T>,
+  ) { }
+
+  public value(guest: GuestType<T>) {
+    this.baseGuestAware.value(guest);
+    return this;
+  }
+
+  public give(value: T) {
+    give(value, this.baseGuest);
+    return this;
+  }
+
+  public pool(): PatronPool<T> {
+    throw Error('No pool in SourceDynamic');
+  }
+}
